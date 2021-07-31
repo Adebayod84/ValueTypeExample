c4fc292a62531487fc1c824eeb3a77cc0dd23555
Adebayo Oduwobi <adebayooduwobi@gmail.com>
2021-07-31 08:10:42 -0500

    Value Type Example

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValueTypeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal loanAmount = 0;
+            decimal loanAmountCopy = 0;
+
+            Console.WriteLine("Please enter a loan amount: ");
+
+            loanAmount = Convert.ToDecimal(Console.ReadLine());
+
+            loanAmountCopy = loanAmount;
+
+            Console.WriteLine($"loanAmount: {loanAmount}, loanAmountCopy: {loanAmountCopy}");
+
+            Console.ReadKey();
+
+            loanAmount += 600;
+
+            Console.WriteLine();
+            Console.WriteLine($"After adding 600 dollars loanAmount: {loanAmount}, loanAmountCopy {loanAmountCopy}");
+
+            Console.ReadKey();
+        }
+    }
+}
